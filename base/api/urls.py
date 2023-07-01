6d6905654e8ae2c6cd9b728267459bf001951cd4
kayegrace14 <kayegrace14@gmail.com>
2023-07-01 12:33:47 +0300

    added model logic

--- base/api/urls.py
+++ base/api/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('login/',views.user_login),
-    path('logout/',views.logout),
+    path('logout/',views.logout_user),
     path('register-batch/', views.register_batch, name='register-batch'),
     path('update-batch/<str:pk>/', views.update_batch, name='update-batch'),
+    path('classify/', views.classify_image, name='classify')
 ]
--- base/api/views.py
+++ base/api/views.py
@@ -5,12 +5,74 @@ import jwt
 from datetime import datetime, timedelta
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework.authtoken.models import Token
 from django.conf import settings
 from .serializers import BatchSerializer
 from base.models import Batch
 
+# imports for the model
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from pydantic import BaseModel
+import numpy as np
+import tensorflow as tf
+import cv2
+
+# secret key
 secret_key = settings.SECRET_KEY
+
+# start of model logic
+
+model = tf.keras.models.load_model('base/api/best_model.h5')
+
+class ClassificationResult(BaseModel):
+    predicted_class: str
+    predicted_probability: float
+
+@csrf_exempt
+def classify_image(request):
+    if request.method == 'POST':
+        # Read and preprocess the image
+
+        image_file = request.FILES['image']
+        img_array = np.frombuffer(image_file.read(), np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        IMG_SIZE = 50
+        new_array = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+        Xt = new_array
+        Xt = np.array(Xt).reshape( IMG_SIZE, IMG_SIZE, 3)
+
+        # Convert image to numpy array and normalize pixel values
+        
+        Xt = Xt/255
+
+        # Perform inference
+        prediction = model.predict(np.expand_dims(Xt, axis=0))
+
+        # Get the predicted class
+        predicted_class = np.argmax(prediction)
+        predicted_probability = np.max(prediction)
+
+        # Define a dictionary to map the class index to a human-readable label
+        class_labels = {
+            0: "Dark",
+            1: "Green",
+            2: "Light",
+            3: "Medium",
+            # Add more class labels as needed
+        }
+
+        # Create the ClassificationResult object for the response
+        result = ClassificationResult(
+            predicted_class=class_labels[predicted_class],
+            predicted_probability=float(predicted_probability)
+        )
+
+        # Return the result as a JSON response
+        return JsonResponse(result.dict())
+    return JsonResponse({'error': 'Invalid request method.'})
+#end of model logic
 
 
 @api_view(['GET', 'PUT', 'POST'])
@@ -46,11 +108,9 @@ def user_login(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def logout(request):
-    django_request = request._request
-    logout(django_request)
+def logout_user(request):
+    logout(request)
     return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
-
 
 
 @api_view(['POST'])
